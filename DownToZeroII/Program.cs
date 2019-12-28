532767b5b3e7c21872f9a27871a52399a080ea22
Cassio <cassiot@gmail.com>
2019-12-28 05:30:55 -0300

    DownToZeroII empty

---
+++ DownToZeroII/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DownToZeroII
+{
+    class Program
+    {
+        static int downToZero(int n)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int result = downToZero(n);
+
+                //textWriter.WriteLine(result);
+            }
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
